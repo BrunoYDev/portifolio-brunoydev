375be19e64f3b277e2348555680ad0f256cb4f6c
Bruno Garcia <bruno.rogues.garcia@gmail.com>
2024-01-30 14:04:15 -0300

    fix: fix screen resolution 768p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,13 +18,13 @@ const Home = async () => {
 
   return (
     <>
-      <main className="body min-h-screen justify-center overflow-hidden">
+      <main className="body h-screen justify-center overflow-x-hidden">
         <section
           id="about"
-          className="flex-col p-0 min-h-screen pt-28 border-b-4 border-grey-100 md:flex md:pt-0 md:flex-wrap md:justify-between md:gap-10"
+          className="flex-col p-0 h-screen pt-28 lg:pt-0 border-b-4 border-grey-100 md:flex md:flex-wrap md:justify-between md:gap-10"
         >
-          <div className="flex-col w-[90%] mx-auto justify-center md:flex md:w-[50%] md:self-start md:mx-0 md:h-[100%] md:ml-28">
-            <div className="flex gap-5 justify-center items-center md:justify-start">
+          <div className="flex-col w-[90%] mx-auto justify-center md:flex md:pt-20 md:w-[50%] md:self-start md:mx-0 md:h-[100%] md:ml-28">
+            <div className="flex gap-5 justify-center items-center md:justify-start ">
               <figure className="flex rounded-full overflow-hidden w-16 h-16 ">
                 <Image
                   src="/profile.jpg"
